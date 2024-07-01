e0f9a527f2ca34236b71d71b4ddfa7dda01e1fb6
Dilip Chauhan <chauhandilip305@gmail.com>
2024-07-01 20:46:40 +0530

    updated dotfiles

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -39,3 +39,25 @@ function Status:render(area)
       table.unpack(Progress:render(area, right:width())),
    }
 end
+
+function Header:tabs()
+   local tabs = #cx.tabs
+   -- Removing following code to show tabs even if it is only 1.
+   -- if tabs == 1 then
+   --    return ui.Line {}
+   -- end
+
+   local spans = {}
+   for i = 1, tabs do
+      local text = i
+      if THEME.manager.tab_width > 2 then
+         text = ya.truncate(text .. " " .. cx.tabs[i]:name(), { max = THEME.manager.tab_width })
+      end
+      if i == cx.tabs.idx then
+         spans[#spans + 1] = ui.Span(" " .. text .. " "):style(THEME.manager.tab_active)
+      else
+         spans[#spans + 1] = ui.Span(" " .. text .. " "):style(THEME.manager.tab_inactive)
+      end
+   end
+   return ui.Line(spans)
+end
