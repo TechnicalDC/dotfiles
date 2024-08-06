012b35e9291810860f0f891ff2feae9466f72e4a
Dilip Chauhan <chauhandilip305@gmail.com>
2024-08-06 09:27:19 +0530

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,7 +12,6 @@ set --export PATH  $PATH $HOME/go/bin/
 set --export PATH  $PATH $HOME/.scripts/
 set --export QT_QUICK_CONTROLS_STYLE Basic
 set --export FZF_DEFAULT_OPTS "--layout=reverse
---preview='batcat {} --color always --style=plain'
 --margin=10%
 --border=rounded
 --height=100%
