f291af03772aa1cd8346eb0664789a40a7296e60
Dilip Chauhan <chauhandilip305@gmail.com>
2024-08-14 22:31:02 +0530

    updated

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -9,32 +9,44 @@ import os
 import subprocess
 import catppuccin
 
-##############
-# XRESOURCES #
-##############
-def read_xresources(prefix):
-    """
-    read settings from xresources
-    """
-    props = {}
-    x = subprocess.run(["xrdb", "-query"], stdout=subprocess.PIPE)
-    lines = x.stdout.decode().split("\n")
-    for line in filter(lambda l: l.startswith(prefix), lines):
-        prop, _, value = line.partition(":\t")
-        props[prop] = value
-    return props
-
-xresources = read_xresources("*")
+catppuccin_colors = {
+   "rosewater" : "#f4dbd6",
+   "flamingo"  : "#f0c6c6",
+   "pink"      : "#f5bde6",
+   "mauve"     : "#c6a0f6",
+   "red"       : "#ed8796",
+   "maroon"    : "#ee99a0",
+   "peach"     : "#f5a97f",
+   "yellow"    : "#eed49f",
+   "green"     : "#a6da95",
+   "teal"      : "#8bd5ca",
+   "sky"       : "#91d7e3",
+   "sapphire"  : "#7dc4e4",
+   "blue"      : "#8aadf4",
+   "lavender"  : "#b7bdf8",
+   "text"      : "#cad3f5",
+   "subtext1"  : "#b8c0e0",
+   "subtext0"  : "#a5adcb",
+   "overlay2"  : "#939ab7",
+   "overlay1"  : "#8087a2",
+   "overlay0"  : "#6e738d",
+   "surface2"  : "#5b6078",
+   "surface1"  : "#494d64",
+   "surface0"  : "#363a4f",
+   "base"      : "#24273a",
+   "mantle"    : "#1e2030",
+   "crust"     : "#181926"
+}
 
 # Quick hack to theme duckduckgo pages
-ddg_theme = 'https://duckduckgo.com/?q={}' # &kj=' + xresources["*.color4"]      # Header color
-# ddg_theme = ddg_theme + '&k7=' + xresources["*.background"]                  # Hover color
-# ddg_theme = ddg_theme + '&k21=' + xresources["*.background"]                 # Background color
+ddg_theme = 'https://duckduckgo.com/?q={}' # &kj=' + xresources["*.color4"]      # catppuccin_colors["mauve"] color
+# ddg_theme = ddg_theme + '&k7=' + xresources["*.catppuccin_colors["base"]"]                  # Hover color
+# ddg_theme = ddg_theme + '&k21=' + xresources["*.catppuccin_colors["base"]"]                 # Background color
 # ddg_theme = ddg_theme + '&kx=' + xresources["*.color5"]                      # URL color
 # ddg_theme = ddg_theme + '&k8=' + xresources["*.color5"]                      # Snippet color
 # ddg_theme = ddg_theme + '&k9=' + xresources["*.color2"]                      # Link color
 # ddg_theme = ddg_theme + '&kaa=' + xresources["*.color2"]                     # Visited link color
-# ddg_theme = ddg_theme + '&ko=s'                                              # Header setting
+# ddg_theme = ddg_theme + '&ko=s'                                              # catppuccin_colors["mauve"] setting
 
 #################
 # MAIN SETTINGS #
@@ -111,103 +123,92 @@ c.statusbar.padding = {"bottom":1, "left":1, "right":1, "top":1}
 ###########################
 # UI COLROS CONFIGURATION #
 ###########################
-
-
-# background = xresources["*.background"]
-# foreground = xresources["*.foreground"]
-# header = xresources["*.color4"]
-# error = xresources["*.color1"]
-# warn = xresources["*.color3"]
-# alt_background = xresources["*.color5"]
-# alt_foreground = "#888888"
-
 # --- Custom CSS ---
 # config.set("content.user_stylesheets","~/.config/qutebrowser/stylesheets/wal-duckduckgo.com.css")
 
 # --- Webpages ---
-# c.colors.webpage.bg = background
+c.colors.webpage.bg = catppuccin_colors["base"]
 
-# if xresources["*.background"] != "#ffffff" or xresources["*.background"] != "#FFFFFF":
-#     c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.enabled = True
 
 # --- Context menu ---
-# c.colors.contextmenu.menu.bg = background
-# c.colors.contextmenu.menu.fg = foreground
-# c.colors.contextmenu.selected.bg = alt_background
-# c.colors.contextmenu.selected.fg = background
+c.colors.contextmenu.menu.bg = catppuccin_colors["base"]
+c.colors.contextmenu.menu.fg = catppuccin_colors["text"]
+c.colors.contextmenu.selected.bg = catppuccin_colors["surface0"]
+c.colors.contextmenu.selected.fg = catppuccin_colors["base"]
 
 # --- Completions ---
-# c.colors.completion.fg = foreground
-# c.colors.completion.odd.bg = background
-# c.colors.completion.even.bg = background
-# c.colors.completion.category.fg = background
-# c.colors.completion.category.bg = header
-# c.colors.completion.category.border.top = header
-# c.colors.completion.category.border.bottom = background
-# c.colors.completion.item.selected.fg = background
-# c.colors.completion.item.selected.bg = alt_background
-# c.colors.completion.item.selected.border.top = alt_background
-# c.colors.completion.item.selected.border.bottom = alt_background
-# c.colors.completion.item.selected.match.fg = background
-# c.colors.completion.match.fg = foreground
-# c.colors.completion.scrollbar.fg = alt_background
-# c.colors.completion.scrollbar.bg = background
+c.colors.completion.fg = catppuccin_colors["text"]
+c.colors.completion.odd.bg = catppuccin_colors["base"]
+c.colors.completion.even.bg = catppuccin_colors["surface0"]
+c.colors.completion.category.fg = catppuccin_colors["base"]
+c.colors.completion.category.bg = catppuccin_colors["mauve"]
+c.colors.completion.category.border.top = catppuccin_colors["mauve"]
+c.colors.completion.category.border.bottom = catppuccin_colors["base"]
+c.colors.completion.item.selected.fg = catppuccin_colors["base"]
+c.colors.completion.item.selected.bg = catppuccin_colors["text"]
+c.colors.completion.item.selected.border.top = catppuccin_colors["surface0"]
+c.colors.completion.item.selected.border.bottom = catppuccin_colors["surface0"]
+c.colors.completion.item.selected.match.fg = catppuccin_colors["base"]
+c.colors.completion.match.fg = catppuccin_colors["green"]
+c.colors.completion.scrollbar.fg = catppuccin_colors["surface1"]
+c.colors.completion.scrollbar.bg = catppuccin_colors["base"]
 
 # --- Downloads ---
-# c.colors.downloads.bar.bg = alt_background
-# c.colors.downloads.error.bg = error
+c.colors.downloads.bar.bg = catppuccin_colors["surface0"]
+c.colors.downloads.error.bg = catppuccin_colors["red"]
 
 # --- Hints ---
-# c.colors.hints.bg = background
-# c.colors.hints.fg = alt_background
-# c.colors.hints.match.fg = foreground
+c.colors.hints.bg = catppuccin_colors["base"]
+c.colors.hints.fg = catppuccin_colors["surface0"]
+c.colors.hints.match.fg = catppuccin_colors["text"]
 
 # --- Messages ---
-# c.colors.messages.info.bg = alt_background
-# c.colors.messages.info.fg = background
-# c.colors.messages.error.bg = error
-# c.colors.messages.warning.bg = warn
-# c.colors.messages.warning.fg = background
+c.colors.messages.info.bg = catppuccin_colors["surface0"]
+c.colors.messages.info.fg = catppuccin_colors["base"]
+c.colors.messages.error.bg = catppuccin_colors["red"]
+c.colors.messages.warning.bg = catppuccin_colors["yellow"]
+c.colors.messages.warning.fg = catppuccin_colors["base"]
 
 # --- Prompts ---
-# c.colors.prompts.bg = background
-# c.colors.prompts.selected.bg = alt_background
-# c.colors.prompts.fg = foreground
+c.colors.prompts.bg = catppuccin_colors["base"]
+c.colors.prompts.selected.bg = catppuccin_colors["surface0"]
+c.colors.prompts.fg = catppuccin_colors["text"]
 
 # --- Statusbar ---
-# c.colors.statusbar.normal.bg = background
-# c.colors.statusbar.insert.fg = background
-# c.colors.statusbar.insert.bg = header
-# c.colors.statusbar.passthrough.bg = background
-# c.colors.statusbar.command.bg = alt_background
-# c.colors.statusbar.command.fg = background
-c.colors.statusbar.url.warn.fg = 'yellow'
+c.colors.statusbar.normal.bg = catppuccin_colors["base"]
+c.colors.statusbar.insert.fg = catppuccin_colors["base"]
+c.colors.statusbar.insert.bg = catppuccin_colors["mauve"]
+c.colors.statusbar.passthrough.bg = catppuccin_colors["base"]
+c.colors.statusbar.command.bg = catppuccin_colors["surface0"]
+c.colors.statusbar.command.fg = catppuccin_colors["text"]
+c.colors.statusbar.url.warn.fg = catppuccin_colors["yellow"]
 
 # --- Tabs ---
-# c.colors.tabs.bar.bg = background
-# c.colors.tabs.odd.bg = background
-# c.colors.tabs.even.bg = background
-# c.colors.tabs.selected.odd.bg = alt_background
-# c.colors.tabs.selected.odd.fg = background
-# c.colors.tabs.selected.even.bg = alt_background
-# c.colors.tabs.selected.even.fg = background
-# c.colors.tabs.pinned.odd.bg = background
-# c.colors.tabs.pinned.even.bg = background
-# c.colors.tabs.pinned.selected.odd.bg = alt_background
-# c.colors.tabs.pinned.selected.even.bg = alt_background
+c.colors.tabs.bar.bg = catppuccin_colors["base"]
+c.colors.tabs.odd.bg = catppuccin_colors["base"]
+c.colors.tabs.even.bg = catppuccin_colors["base"]
+c.colors.tabs.selected.odd.bg = catppuccin_colors["surface0"]
+c.colors.tabs.selected.odd.fg = catppuccin_colors["base"]
+c.colors.tabs.selected.even.bg = catppuccin_colors["surface0"]
+c.colors.tabs.selected.even.fg = catppuccin_colors["base"]
+c.colors.tabs.pinned.odd.bg = catppuccin_colors["base"]
+c.colors.tabs.pinned.even.bg = catppuccin_colors["base"]
+c.colors.tabs.pinned.selected.odd.bg = catppuccin_colors["surface0"]
+c.colors.tabs.pinned.selected.even.bg = catppuccin_colors["surface0"]
 
 
 ################
 # FONT SETTING #
 ################
 
-c.fonts.default_family = '11px "JetBrainsMonoNL Nerd Font"'
-c.fonts.default_size = '11px'
-c.fonts.contextmenu = '11px "JetBrainsMonoNL Nerd Font"'
-c.fonts.completion.entry = '11px "JetBrainsMonoNL Nerd Font"'    # Font used in the completion widget.
-c.fonts.debug_console = '11px "JetBrainsMonoNL Nerd Font"'       # Font used for the debugging console.
+c.fonts.default_family = '14px "JetBrainsMonoNL Nerd Font"'
+c.fonts.default_size = '14px'
+c.fonts.contextmenu = '14px "JetBrainsMonoNL Nerd Font"'
+c.fonts.completion.entry = '14px "JetBrainsMonoNL Nerd Font"'    # Font used in the completion widget.
+c.fonts.debug_console = '14px "JetBrainsMonoNL Nerd Font"'       # Font used for the debugging console.
 c.fonts.prompts = 'default_size FantasqueSansMono Nerd Font'       # Font used for prompts.
-c.fonts.statusbar = '11px "JetBrainsMonoNL Nerd Font"'           # Font used in the statusbar.
+c.fonts.statusbar = '14px "JetBrainsMonoNL Nerd Font"'           # Font used in the statusbar.
 
 
 ################################
