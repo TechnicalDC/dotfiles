49c7c52063fe1b723d80603f864ef077240bcc29
Dilip Chauhan <chauhandilip305@gmail.com>
2024-08-15 11:30:05 +0530

    updated

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -73,7 +73,10 @@ c.url.searchengines = {
 # c.url.start_pages = '/home/dilip/.cache/StartTree/index.html'
 c.content.images = True
 c.content.javascript.enabled = True
-c.completion.web_history.max_items = 0
+c.content.autoplay = False
+c.content.notifications.presenter = "auto"
+# c.content.private_browsing = True
+c.completion.web_history.max_items = 15
 
 # Valid values:
 #   - all: Accept all cookies.
@@ -95,9 +98,9 @@ c.content.headers.user_agent = 'Mozilla/5.0 ({os_info}) AppleWebKit/{webkit_vers
 c.completion.shrink = True
 
 # --- File Select ---
-config.set("fileselect.handler", "external")
-config.set("fileselect.single_file.command" , ["st", "-n", "filechooser", "-e", "ranger", "--choosefile", "{}"])
-config.set("fileselect.multiple_files.command", ["st", "-n", "filechooser", "-e", "ranger", "--choosefiles", "{}"])
+# config.set("fileselect.handler", "external")
+# config.set("fileselect.single_file.command" , ["st", "-n", "filechooser", "-e", "ranger", "--choosefile", "{}"])
+# config.set("fileselect.multiple_files.command", ["st", "-n", "filechooser", "-e", "ranger", "--choosefiles", "{}"])
 
 c.content.pdfjs = True
 
@@ -159,15 +162,16 @@ c.colors.downloads.bar.bg = catppuccin_colors["surface0"]
 c.colors.downloads.error.bg = catppuccin_colors["red"]
 
 # --- Hints ---
-c.colors.hints.bg = catppuccin_colors["base"]
-c.colors.hints.fg = catppuccin_colors["surface0"]
-c.colors.hints.match.fg = catppuccin_colors["text"]
+c.colors.hints.bg = catppuccin_colors["yellow"]
+c.colors.hints.fg = catppuccin_colors["base"]
+c.colors.hints.match.fg = catppuccin_colors["red"]
 
 # --- Messages ---
 c.colors.messages.info.bg = catppuccin_colors["surface0"]
 c.colors.messages.info.fg = catppuccin_colors["base"]
 c.colors.messages.error.bg = catppuccin_colors["red"]
-c.colors.messages.warning.bg = catppuccin_colors["yellow"]
+c.colors.messages.error.fg = catppuccin_colors["text"]
+c.colors.messages.warning.bg = catppuccin_colors["peach"]
 c.colors.messages.warning.fg = catppuccin_colors["base"]
 
 # --- Prompts ---
@@ -177,21 +181,26 @@ c.colors.prompts.fg = catppuccin_colors["text"]
 
 # --- Statusbar ---
 c.colors.statusbar.normal.bg = catppuccin_colors["base"]
+c.colors.statusbar.normal.fg = catppuccin_colors["text"]
 c.colors.statusbar.insert.fg = catppuccin_colors["base"]
-c.colors.statusbar.insert.bg = catppuccin_colors["mauve"]
+c.colors.statusbar.insert.bg = catppuccin_colors["green"]
 c.colors.statusbar.passthrough.bg = catppuccin_colors["base"]
 c.colors.statusbar.command.bg = catppuccin_colors["surface0"]
 c.colors.statusbar.command.fg = catppuccin_colors["text"]
-c.colors.statusbar.url.warn.fg = catppuccin_colors["yellow"]
+c.colors.statusbar.command.private.bg = catppuccin_colors["surface0"]
+c.colors.statusbar.command.private.fg = catppuccin_colors["text"]
+c.colors.statusbar.private.bg = catppuccin_colors["yellow"]
+c.colors.statusbar.private.fg = catppuccin_colors["base"]
+c.colors.statusbar.url.warn.fg = catppuccin_colors["peach"]
 
 # --- Tabs ---
 c.colors.tabs.bar.bg = catppuccin_colors["base"]
 c.colors.tabs.odd.bg = catppuccin_colors["base"]
 c.colors.tabs.even.bg = catppuccin_colors["base"]
 c.colors.tabs.selected.odd.bg = catppuccin_colors["surface0"]
-c.colors.tabs.selected.odd.fg = catppuccin_colors["base"]
+c.colors.tabs.selected.odd.fg = catppuccin_colors["text"]
 c.colors.tabs.selected.even.bg = catppuccin_colors["surface0"]
-c.colors.tabs.selected.even.fg = catppuccin_colors["base"]
+c.colors.tabs.selected.even.fg = catppuccin_colors["text"]
 c.colors.tabs.pinned.odd.bg = catppuccin_colors["base"]
 c.colors.tabs.pinned.even.bg = catppuccin_colors["base"]
 c.colors.tabs.pinned.selected.odd.bg = catppuccin_colors["surface0"]
@@ -207,7 +216,7 @@ c.fonts.default_size = '14px'
 c.fonts.contextmenu = '14px "JetBrainsMonoNL Nerd Font"'
 c.fonts.completion.entry = '14px "JetBrainsMonoNL Nerd Font"'    # Font used in the completion widget.
 c.fonts.debug_console = '14px "JetBrainsMonoNL Nerd Font"'       # Font used for the debugging console.
-c.fonts.prompts = 'default_size FantasqueSansMono Nerd Font'       # Font used for prompts.
+c.fonts.prompts = 'default_size JetBrainsMonoNL Nerd Font'       # Font used for prompts.
 c.fonts.statusbar = '14px "JetBrainsMonoNL Nerd Font"'           # Font used in the statusbar.
 
 
