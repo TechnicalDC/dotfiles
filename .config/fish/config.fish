715844c8ccb39c7c17bbaf5a7038a920b4157bb7
Dilip Chauhan <chauhandilip305@gmail.com>
2025-01-31 01:04:59 +0530

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,6 @@ set --export PATH  $PATH $HOME/.scripts/
 set --export PATH  $PATH $HOME/.local/bin/
 set --export PATH  $PATH $HOME/.cargo/bin/
 set --export PATH  $PATH $HOME/go/bin/
-set --export PATH  $PATH $HOME/.scripts/
 set --export QT_QUICK_CONTROLS_STYLE Basic
 set --export FZF_DEFAULT_OPTS "--layout=reverse
 --margin=10%
@@ -29,7 +28,7 @@ set --export _ZO_FZF_OPTS "--layout=reverse
 --height=100%
 --prompt=' '
 --pointer='>>'
---color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=bg+:#111111,bg:#111111,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a"
