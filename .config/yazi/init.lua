f670bad3bee7f4a09a514863d4ac10c4c4f30430
Dilip Chauhan <chauhandilip305@gmail.com>
2025-03-08 20:05:38 +0530

    updated yazi's `init.lua` file

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -7,32 +7,28 @@ require("session"):setup {
 	sync_yanked = true,
 }
 
-function Status:name()
-   local h = cx.active.current.hovered
-   if not h then
-      return ui.Span("")
-   end
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
 
-   local linked = ""
-   if h.link_to ~= nil then
-      linked = " -> " .. tostring(h.link_to)
-   end
-   return ui.Span(" " .. h.name .. linked)
-end
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ""
+	end
 
-function Status:owner()
-   local h = cx.active.current.hovered
-   if h == nil or ya.target_family() ~= "unix" then
-      return ui.Line {}
-   end
-
-   return ui.Line {
-      ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
-      ui.Span(":"),
-      ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
-      ui.Span(" "),
-   }
-end
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	}
+end, 500, Status.RIGHT)
 
 function Status:render(area)
    self.area = area
