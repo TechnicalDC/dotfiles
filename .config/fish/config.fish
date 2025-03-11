b14a94c31bcbdc0a8f63a348cbfa0f28002bbd02
Dilip Chauhan <chauhandilip305@gmail.com>
2025-03-11 22:46:58 +0530

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ set --export SHELL /usr/bin/fish
 set --export ANDROID_HOME $XDG_DATA_HOME/android
 set --export EDITOR "nvim"
 set --export VISUAL "nvim"
-set --export TERMINAL "ghostty"
+set --export TERMINAL "kitty"
 set --export PGER "less"
 set --export MANPAGER "nvim +Man!"
 set --export BROWSER "qutebrowser"
