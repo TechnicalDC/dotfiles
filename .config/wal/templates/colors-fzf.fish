60fb1432a5304d7c167f1cd21f748ceb7c5abf2d
Dilip Chauhan <chauhandilip305@gmail.com>
2025-03-23 15:11:04 +0530

    updated

---
+++ .config/wal/templates/colors-fzf.fish
@@ -0,0 +1,4 @@
+set --export FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS "--color=fg:{foreground},fg+:{background},bg:-1,bg+:{background} \
+--color=hl:{color3},hl+:{color1},info:{color7},marker:{color3} \
+--color=prompt:{color4} \
+--color=border:{color6},label:#ef596f,query:{foreground}"
