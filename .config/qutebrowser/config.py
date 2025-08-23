01720a84998a70cc6f2e3636d30c2c82bb67b2f6
Dilip Chauhan <chauhandilip305@gmail.com>
2025-08-23 15:21:18 +0530

    updated

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -172,8 +172,8 @@ c.colors.tabs.pinned.selected.even.bg = mini["bg_mid2"]
 ################
 # FONT SETTING #
 ################
-font = '20px "FantasqueSansM Nerd Font"'
-# font = '18px "FantasqueSa Nerd Font"'
+# font = '20px "FantasqueSansM Nerd Font"'
+font = '18px "JetBrainsMonoNL Nerd Font"'
 c.fonts.default_family   = font
 c.fonts.default_size     = '20px'
 c.fonts.contextmenu         = font
