e54e315b6babb9054238cd0a613897c9cc06a1c8
Dilip Chauhan <chauhandilip305@gmail.com>
2025-10-31 00:48:12 +0530

    updated all

---
+++ .config/elephant/menus/wallpaper.lua
@@ -0,0 +1,39 @@
+Name = "wallpaper"
+NamePretty = "Wallpaper"
+Icon = "applications-other"
+Cache = true
+Action = "notify-send %VALUE%"
+HideFromProviderlist = false
+Description = "Wallpaper Provider"
+SearchName = true
+
+function GetEntries()
+    local entries = {}
+    local wallpaper_dir = "/home/dilip/Pictures/wallpapers/"
+
+    local handle = io.popen("find '" ..
+        wallpaper_dir ..
+        "' -maxdepth 1 -type f -name '*.jpg' -o -name '*.jpeg' -o -name '*.png' -o -name '*.gif' -o -name '*.bmp' -o -name '*.webp' 2>/dev/null")
+    if handle then
+        for line in handle:lines() do
+           print(line)
+            local filename = line:match("([^/]+)$")
+            if filename then
+                table.insert(entries, {
+                    Text = filename,
+                    Subtext = "wallpaper",
+                    Value = line,
+                    Actions = {
+                        up = "notify-send up",
+                        down = "notify-send down",
+                    },
+                    -- Preview = line,
+                    Icon = line
+                })
+            end
+        end
+        handle:close()
+    end
+
+    return entries
+end
