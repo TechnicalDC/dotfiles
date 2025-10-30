9eea01ded636fea18584801d4d986a7071c84ce3
Dilip Chauhan <chauhandilip305@gmail.com>
2025-10-31 01:27:46 +0530

    updated

--- .config/walker/launch.sh
+++ .config/walker/launch.sh
@@ -11,6 +11,17 @@ else
     elephant &
 fi
 
+# Check if elephant is running
+if pgrep "walker --gapplication-service" > /dev/null
+then
+    # Send this diagnostic message to Standard Error (2>&1)
+    echo ":: Walker GApplication Service is running. ::" >&2
+else
+    # Send this diagnostic message to Standard Error (2>&1)
+    echo ":: Walker GApplication Service is NOT running. ::" >&2
+    walker --gapplication-service &
+fi
+
 # Send this diagnostic message to Standard Error (2>&1)
 echo ":: Launching walker with arguments: $* ::" >&2
 
